1d5fc9bcfa6e900b3d0dfaa9d5aa2cee7195c5d3
Jonathan Young <jonathanyoung7718@gmail.com>
2024-04-26 16:08:16 -0500

    domain finished

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// Domain
 var cells = document.getElementsByTagName('td');
+// Global State
 var isMouseDown = false;
-var isPencil = true;
+// User state
+var isEraser = false;
+var isObstacle = true;
+var isGoal = false;
+var isStart = false;
+// Domain state
+var goalCreated = false;
+var startCreated = false;
 
 document.addEventListener('mousedown', () => {
   isMouseDown = true;
@@ -10,19 +19,91 @@ document.addEventListener('mouseup', () => {
   isMouseDown = false;
 })
 
+// Dragging
 for (var i = 0; i < cells.length; i++) {
   cells[i].addEventListener('mousemove', (e) => {
     if (!isMouseDown) return;
-    if (isPencil) {
+
+    if (e.target.style.backgroundColor === 'green') goalCreated = false;
+    else if (e.target.style.backgroundColor === 'orange') startCreated = false;
+
+    if (isObstacle) {
       e.target.style.backgroundColor = 'red';
-    } else {
+    } 
+    else if (isEraser) {
       e.target.style.backgroundColor = 'dodgerblue';
     }
   });
 }
 
-document.getElementById('pencil').addEventListener('click', (e) => {
-  isPencil = isPencil ? false : true;
-  document.getElementById(e.target.id).innerHTML = isPencil ? 'Pencil' : 'Eraser';
-  console.log(e.target.id)
+// Clicking
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', (e) => {
+    if (e.target.style.backgroundColor === 'green') goalCreated = false;
+    else if (e.target.style.backgroundColor === 'orange') startCreated = false;
+
+    if (isEraser) {
+      e.target.style.backgroundColor = 'dodgerblue';
+    }
+    else if (isObstacle) {
+      e.target.style.backgroundColor = 'red';
+    }
+    else if (isGoal) {
+      if (goalCreated) {
+        alert('Goal already created');
+        return;
+      }
+      e.target.style.backgroundColor = 'green';
+      goalCreated = true;
+    }
+    else if (isStart) {
+      if (startCreated) {
+        alert('Start already created');
+        return;
+      }
+      e.target.style.backgroundColor = 'orange';
+      startCreated = true;
+    }
+  })
+}
+
+// Eraser
+document.getElementById('eraser').addEventListener('click', () => {
+  isEraser = true;
+  isObstacle = false;
+  isStart = false;
+  isGoal = false;
+})
+
+// Obstacle
+document.getElementById('obstacle').addEventListener('click', () => {
+  isEraser = false;
+  isObstacle = true;
+  isStart = false;
+  isGoal = false;
+})
+
+// Start
+document.getElementById('start').addEventListener('click', () => {
+  isEraser = false;
+  isObstacle = false; 
+  isStart = true;
+  isGoal = false;
+})
+
+// Goal
+document.getElementById('goal').addEventListener('click', () => {
+  isEraser = false;
+  isObstacle = false;
+  isStart = false;
+  isGoal = true;
+})
+
+// Reset
+document.getElementById('reset').addEventListener('click', () => {
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = "dodgerblue";
+  }
+  goalCreated = false;
+  startCreated = false;
 })
