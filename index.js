30c2b84c985f3aea28f35d6fc49f826c0c4ae568
Jonathan Young <jonathanyoung7718@gmail.com>
2024-05-02 16:52:28 -0500

    got dijkstras roughly working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Global State
+var darkMode = false;
 var isMouseDown = false;
 // User state
 var isEraser = false;
@@ -11,8 +12,53 @@ var goalPosition = null;
 var startCreated = false;
 var startPosition = null;
 
+const width = 30;
+const height = 20;
+
+// Priority Queue
+class Queue {
+  constructor() {
+    this.elements = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+  enqueue(element) {
+    this.elements[this.tail] = element;
+    this.tail++;
+  }
+  dequeue() {
+    const item = this.elements[this.head];
+    delete this.elements[this.head];
+    this.head++;
+    return item;
+  }
+  peek() {
+    return this.elements[this.head];
+  }
+  get length() {
+    return this.tail - this.head;
+  }
+  get isEmpty() {
+    return this.length === 0;
+  }
+}
+
 // Initialize Domain on load
-makeDomain(30, 20);
+makeDomain(width, height);
+
+// Dark mode function
+function toggleDarkMode() {
+  darkMode = !darkMode;
+  if (darkMode) {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    document.getElementsByTagName('table')[0].style.borderColor = 'white';
+  } else {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+    document.getElementsByTagName('table')[0].style.borderColor = 'black';
+  }
+}
 
 // Global event listeners
 document.addEventListener('mousedown', () => {
@@ -130,10 +176,13 @@ document.getElementById('goal-position').addEventListener('click', () => {
 document.getElementById('reset').addEventListener('click', () => {
   const cells = document.getElementsByTagName('td');
   for (var i = 0; i < cells.length; i++) {
-    cells[i].classList.remove('obstacle');
-    cells[i].classList.remove('goal');
-    cells[i].classList.remove('start');
-    cells[i].style.backgroundColor = "dodgerblue";
+    const cell = cells[i];
+    cell.classList.remove('obstacle');
+    cell.classList.remove('goal');
+    cell.classList.remove('start');
+    cell.classList.remove('guess');
+    cell.classList.remove('found');
+    cell.style.backgroundColor = "dodgerblue";
   }
   goalCreated = false;
   startCreated = false;
@@ -146,6 +195,13 @@ document.getElementById('run-algorithm').addEventListener('click', async () => {
   await findPath(start, goal);
 })
 
+document.getElementById('dijkstras').addEventListener('click', async () => {
+  const start = getNumberCoords(startPosition);
+  const goal = getNumberCoords(goalPosition);
+
+  await dijkstras(start, goal);
+})
+
 // Algorithms
 // Direct path to goal
 async function findPath(start, end) {
@@ -155,39 +211,19 @@ async function findPath(start, end) {
   var endY = end[1];
 
   while (currX !== endX || currY !== endY) {
-    if (currX > endX && currY < endY) {
-      // move up and left
-      currX--;
-      currY++;
-    }
-    else if (currX < endX && currY < endY) {
-      // move down and left
-      currX++;
-      currY++;
-    }
-    else if (currX < endX && currY > endY) {
-      // move down and right
-      currX++;
-      currY--;
-    }
-    else if (currX > endX && currY > endY) {
-      // move up and right
-      currX--;
-      currY--;
-    }
-    else if (currX > endX) {
+    if (currX > endX && !checkValid([currX - 1, currY])) {
       // move up
       currX--;
     }
-    else if (currX < endX) {
+    else if (currX < endX && !checkValid([currX + 1, currY])) {
       // move down
       currX++;
     }
-    else if (currY > endY) {
+    else if (currY > endY && !checkValid([currX, currY - 1])) {
       // move right
       currY--;
     }
-    else if (currY < endY) {
+    else if (currY < endY && !checkValid([currX, currY + 1])) {
       // move left
       currY++;
     }
@@ -196,6 +232,52 @@ async function findPath(start, end) {
     await new Promise(resolve => setTimeout(resolve, 100));
   }
   alert('Goal Reached!');
+}
+
+async function dijkstras(start, end) {
+  const startString = getStringCoords(start);
+  const endString = getStringCoords(end);
+  // Queue of string coords
+  const q = new Queue();
+  // Set guess distance for start
+  document.getElementById(startString).classList.add('guess');
+  q.enqueue(startString);
+
+  while (!q.isEmpty) {
+    // Get string coords from queue
+    const current = q.dequeue();
+    // Add shortest path
+    document.getElementById(current).classList.add('found');
+    // Check if current is the goal
+    if (current === endString) {
+      alert('Goal Reached!');
+      return;
+    }
+    // Get array coords from string coords
+    const currentCoords = getNumberCoords(current);
+    // Check neighbors
+    const neighbors = [
+      [currentCoords[0] - 1, currentCoords[1]],
+      [currentCoords[0] + 1, currentCoords[1]],
+      [currentCoords[0], currentCoords[1] - 1],
+      [currentCoords[0], currentCoords[1] + 1]
+    ];
+    // Visit neighbors
+    for (var i = 0; i < neighbors.length; i++) {
+      if (!checkValid(neighbors[i])) continue;
+      const neighbor = document.getElementById(getStringCoords(neighbors[i]));
+      if (neighbor.classList.contains('found')) continue;
+      if (neighbor.classList.contains('guess')) continue;
+      // check if neighbor is goal
+      if (getStringCoords(neighbors[i]) === endString) {
+        alert('Goal Reached!');
+        return;
+      }
+      neighbor.classList.add('guess');
+      q.enqueue(getStringCoords(neighbors[i]));
+    }
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
 }
 
 
@@ -209,4 +291,12 @@ function getNumberCoords(coords) {
 function getStringCoords(coords) {
   const stringCoords = coords.join(',');
   return stringCoords;
+}
+
+function checkValid(coords) {
+  const cell = document.getElementById(getStringCoords(coords));
+  if (coords[0] < 0 || coords[0] >= height) return false;
+  if (coords[1] < 0 || coords[1] >= width) return false;
+  if (cell.classList.contains('obstacle')) return false;
+  return true;
 }
